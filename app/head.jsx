073d53338bb9d3d666676e542c2aa2d0c065bc50
Zhivko Andreev <zhia@yousee.dk>
2022-12-05 17:48:24 +0100

    asd

--- app/head.jsx
+++ app/head.jsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Vacations</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon test" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   );
 }
