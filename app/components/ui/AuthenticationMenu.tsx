e84060e97d102c522b41cb30425f41e399cb566f
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-12-13 00:42:31 +0100

    c

--- app/components/ui/AuthenticationMenu.tsx
+++ app/components/ui/AuthenticationMenu.tsx
@@ -7,13 +7,19 @@ const AuthenticationMenu = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [isNotLoggedIn, setNotNotLoggedIn] = useState(true);
 
+  const signOut = () => {
+    console.log("user signed out !");
+  };
+
   return (
     <>
       {isLoggedIn && <Link href="/login">Login</Link>}
       {isNotLoggedIn && (
         <>
           <h5>Hi Jay !</h5>
-          <Link href="/login">Log Out</Link>
+          <Link href="/login" onClick={signOut}>
+            Log Out
+          </Link>
         </>
       )}
     </>
