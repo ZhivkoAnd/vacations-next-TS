04879bc337c4430f5afccd56c5efcc47b59a0eb9
Zhivko Andreev <zhia@yousee.dk>
2023-01-24 13:15:58 +0100

    changes

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,6 +1,11 @@
 "use client"
 
+import {useState} from "react"
+
 const Booking = () => {
+
+    const [count, setCount] = useState(0);
+    const [cart, setCart] = useState([])
 
 
 const vacations = [
@@ -21,15 +26,30 @@ const vacations = [
 ]
 
 
-const addToCart = (city: any) => {
-  console.log(city)
+const addToCart = () => {
+  console.log(cart)
 }
+
+const Increment = () => {
+  setCount(count => count + 1)
+  setCart((a) => [...a, {}])
+}
+
+const Decrement = () => {
+    setCount(count => count - 1)
+  }
+  
 
   return (
     <div className="container">
     {vacations.map((city)=> {
-        return <div key={city.city}><button onClick={() =>addToCart(city)}>{city.city}</button><div>{city.price}</div></div>
+        return <div key={city.city}>
+            <div>{city.city}</div>
+            <div>{city.price}</div>
+            <button onClick={Increment}>+</button><div>Seats{count}</div><button onClick={Decrement}>-</button>
+            </div>
     })}
+    <button onClick={addToCart}>Submit</button>
     </div>
   );
 };
