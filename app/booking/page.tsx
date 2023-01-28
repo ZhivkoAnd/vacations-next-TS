7eb81bd9029ab63ae44fe68062394d77a82e92e3
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-01-28 12:38:51 +0100

    fixing cart

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,57 +1,67 @@
-"use client"
+"use client";
 
-import {useState} from "react"
+import { useState } from "react";
 
 const Booking = () => {
+  const [cartItems, setCartItems] = useState([]);
 
-    const [count, setCount] = useState(0);
-    const [cart, setCart] = useState([])
-
-
-const vacations = [
+  const vacations = [
     {
-    id: 1,
-    city: "copenhagen",
-    price: 230
+      id: 1,
+      city: "copenhagen",
+      price: 230,
     },
     {
-        id: 2,
-    city: "Sofia",
-    price: 100
+      id: 2,
+      city: "Sofia",
+      price: 100,
     },
     {
-        id: 3,
-            city: "Bucharest",
-            price: 50
-            },
+      id: 3,
+      city: "Bucharest",
+      price: 50,
+    },
+  ];
 
-]
+  const add = (product: any) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+    console.log("jej");
+  };
 
-
-const addToCart = () => {
-  console.log(cart)
-}
-
-const Increment = () => {
-  setCount(count => count + 1)
-  setCart((a) => [...a, {city: count}])
-}
-
-const Decrement = () => {
-    setCount(count => count - 1)
-  }
-  
+  console.log(cartItems);
 
   return (
     <div className="container">
-    {vacations.map((city)=> {
-        return <div key={city.city}>
+      {vacations.map((city) => {
+        return (
+          <div key={city.city}>
             <div>{city.city}</div>
             <div>{city.price}</div>
-            <button onClick={Increment}>+</button><div>Seats{count}</div><button onClick={Decrement}>-</button>
-            </div>
-    })}
-    <button onClick={addToCart}>Submit</button>
+            <button onClick={() => add(city)}>+</button>
+          </div>
+        );
+      })}
+      <div>Basket</div>
+      <div>
+        {!cartItems.length ? (
+          <div>Empty</div>
+        ) : (
+          <div>
+            {cartItems.map((e) => {
+              return <div key={e.id}>{e.city}</div>;
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
