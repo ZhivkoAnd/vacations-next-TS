d22ac9d7e09429f1bf9daa63a4679de038c4be57
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-01-29 12:30:46 +0100

    changes

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,32 +1,43 @@
 "use client";
-
+import { FetchVacationsClient } from "../components/utils/FetchQueryClient";
+import Image from "next/image";
 import { useState } from "react";
 
 const Booking = () => {
   const [cartItems, setCartItems] = useState([]);
+  const { data, isLoading } = FetchVacationsClient();
+
+  console.log(data?.items[0].fields);
 
   interface Product {
     id: number;
-    city: string;
+    title: string;
     price: number;
+    image?: string;
     qty?: number;
   }
 
   const vacations = [
     {
       id: 1,
-      city: "copenhagen",
+      title: "copenhagen",
       price: 230,
+      image:
+        "https://www.visitdenmark.com/sites/visitdenmark.com/files/styles/article_teaser/public/2019-03/Nyhavn%20K%C3%B8benhavn_43397.jpg?h=266c594a&itok=1ucaad7M",
     },
     {
       id: 2,
-      city: "Sofia",
+      title: "Sofia",
       price: 100,
+      image:
+        "https://theweekendfox.com/wp-content/uploads/2021/07/Bulgaria-Post-1-Cathedral-3.jpg",
     },
     {
       id: 3,
-      city: "Bucharest",
+      title: "Bucharest",
       price: 50,
+      image:
+        "https://static01.nyt.com/images/2018/11/18/travel/18Hours-Bucharest1/18Hours-Bucharest1-articleLarge-v3.jpg?quality=75&auto=webp&disable=upscale",
     },
   ];
 
@@ -41,7 +52,6 @@ const Booking = () => {
     } else {
       setCartItems([...cartItems, { ...product, qty: 1 }]);
     }
-    console.log("jej");
   };
 
   const remove = (product: any) => {
@@ -55,23 +65,27 @@ const Booking = () => {
     } else {
       setCartItems([...cartItems, { ...product, qty: 1 }]);
     }
-    console.log("jej");
   };
-
-  console.log(cartItems);
 
   return (
     <div className="container">
-      {vacations.map((city) => {
-        return (
-          <div key={city.city}>
-            <div>{city.city}</div>
-            <div>{city.price}</div>
-            <button onClick={() => add(city)}>+</button>
-            <button onClick={() => remove(city)}>-</button>
-          </div>
-        );
-      })}
+      <div className="vacation-panels">
+        {vacations.map((city) => {
+          return (
+            <div className="vacation-panel" key={city.title}>
+              <img
+                src={city.image}
+                className="vacation-panel__image"
+                alt="city thumbnail"
+              />
+              <h2 className="vacation-panel__title"> {city.title}</h2>
+              <h2 className="vacation-panel__title"> ${city.price}</h2>
+              <button onClick={() => add(city)}>+</button>
+              <button onClick={() => remove(city)}>-</button>
+            </div>
+          );
+        })}
+      </div>
       <div>Basket</div>
       <div>
         {!cartItems.length ? (
@@ -81,8 +95,8 @@ const Booking = () => {
             {cartItems.map((e) => {
               return (
                 <div key={e.id}>
-                  {e.city}...
-                  {e.price}...
+                  {e.title}...
+                  {e.price * e.qty}...
                   {e.qty}...
                 </div>
               );
