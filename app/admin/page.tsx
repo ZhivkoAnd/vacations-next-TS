e7a48a4a93dc3eec6a7773530e79162c66abadcb
Zhivko Andreev <zhia@yousee.dk>
2023-02-03 09:57:09 +0100

    asd

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,15 +2,39 @@
 
 import { FetchBookings } from "../components/utils/FetchQueryClient";
 
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image?: string;
+  qty?: number;
+}
+
 const Admin = () => {
 
   const { data, isLoading } = FetchBookings();
   console.log(data)
 
     return (
-      <>
-      Hello
-      </>
+      <div className="container">
+      <div className="vacation-panels">
+        {data?.map((city: Product) => {
+          return (
+            <div className="vacation-panel" key={city.title}>
+              <img
+                src={city.image}
+                className="vacation-panel__image"
+                alt="city thumbnail"
+              />
+              <h2 className="vacation-panel__title"> {city.title}</h2>
+              <h2 className="vacation-panel__title"> Price: ${city.price}</h2>
+              <div className="vacation-panel__shop"> 
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
     );
   };
   
