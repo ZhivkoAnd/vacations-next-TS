95876febb42973cb6b23e004b6edaea27f0ae86a
Zhivko Andreev <zhia@yousee.dk>
2023-02-06 09:49:01 +0100

    asd

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,32 @@
 "use client";
 
+import React, {useState} from "react";
 
-const Admin = () => {
+const Testing = () => {
 
-    return (
-    <div>Hello from CRUD</div>
-    );
-  };
-  
-  export default Admin;
-  
+  const [task, setTask] = useState("")
+
+  const addTask = () => {
+   
+  }
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault()
+    console.log(e)
+    addTask();
+    setTask("")
+  }
+
+  return (
+    <>  
+    <form onSubmit={handleFormSubmit}>
+      {/* <input type="text" id="task" className="input" onInput={(e) => setTask(e.target.value)} required autoFocus maxLength={60} placeholder="Enter text"/> */}
+      {/* <label htmlFor="task" className="label">Enter Task</label> */}
+      <button className="btn" aria-label="Add task" type="submit">Add</button>
+    </form>
+    </>
+  );
+};
+
+export default Testing;
+ 
