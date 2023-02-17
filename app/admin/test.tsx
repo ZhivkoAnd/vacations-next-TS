cdb637c72503a1b8031f52486b34da08a1e33aaa
Zhivko Andreev <zhia@yousee.dk>
2023-02-17 09:36:59 +0100

    asd

--- app/admin/test.tsx
+++ app/admin/test.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import AdminProductList from "../components/ui/AdminProductList";
 
@@ -16,19 +16,117 @@ const QueryAPI = () => {
     return response.json();
   };
 
-  const {data, isLoading} = useQuery(["bookingss"], fetchBookingQuery);
+  const fetchBookings = useQuery(["bookings"], fetchBookingQuery, {
+    onSuccess(data) {
+      setFilters(data.cities);
+    },
+  });
 
-  const [filtered, setFiltered] = useState(data)
+  const createData = async (data: {}) => {
+    const response = await fetch("http://localhost:3000/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  };
 
+  const [filterss, setFilters] = useState(fetchBookings.data?.cities);
 
-useEffect(()=>{
-  setFiltered(data)
-},[data])
+  const newMutation = useMutation(createData, {
+    onSuccess: (data) => {
+      console.log("Success!", data);
+    },
+  });
 
-  console.log(filtered)
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const data = { title, price, image };
+    await newMutation.mutateAsync(data);
+    queryClient.invalidateQueries(["bookings"]);
+  };
+
+  const setFilterDateAscending = () => {
+    setFilters(
+      [...fetchBookings.data.cities].sort((a: any, b: any) => a.price - b.price)
+    );
+  };
+
+  const setFilterDateDescending = () => {
+    setFilters(
+      [...fetchBookings.data.cities].sort((a: any, b: any) => b.price - a.price)
+    );
+  };
+
+  const setFilterTitleAscending = () => {
+    setFilters(
+      [...fetchBookings.data.cities].sort((a: any, b: any) =>
+        a.title.localeCompare(b.title)
+      )
+    );
+  };
+
+  const setFilterTitleDescending = () => {
+    setFilters(
+      [...fetchBookings.data.cities].sort((a: any, b: any) =>
+        b.title.localeCompare(a.title)
+      )
+    );
+  };
+
+  // const removeProduct = async (id: number) => {
+  //   await mutateAsync(id);
+  // };
+
+  if (fetchBookings.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (fetchBookings.isError) {
+    return <div>There was an error</div>;
+  }
 
   return (
     <div className="container">
+      <AdminProductList data={filterss} />
+      <h1>Add new vacation</h1>
+      <button onClick={setFilterDateAscending}>filter THIS</button>
+      <button onClick={setFilterDateDescending}>filter THIS</button>
+      <button onClick={setFilterTitleAscending}>filter THIS</button>
+      <button onClick={setFilterTitleDescending}>filter THIS</button>
+
+      <form className="container" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input
+            value={price}
+            type="number"
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Image</label>
+          <input
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            className="form-control"
+          ></input>
+        </div>
+        <button type="submit" disabled={newMutation.isLoading}>
+          Add vacation
+        </button>
+      </form>
     </div>
   );
 };
